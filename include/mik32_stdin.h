92fcc06ccce63da03dc4c9644d39024d88bc9eb7
Barsy-Barsevich <barsotion@yandex.ru>
2025-06-21 01:26:06 +0500

    non-blocking receive works

--- include/mik32_stdin.h
+++ include/mik32_stdin.h
@@ -2,9 +2,16 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "usart_transaction.h"
 
-#define MIK32STDIN_BUFFER_SIZE      10
+// #define MIK32STDIN_USE_MALLOC
 
-void mik32_stdin_init(void);
+#define MIK32STDIN_BUFSIZE_DEFAULT      5
+
+void mik32_stdin_init(UART_TypeDef *host);
+void mik32_stdin_enable_blocking(void);
+void mik32_stdin_disable_blocking(void);
+uint32_t mik32_stdin_get_buffer_size(void);
+void mik32_stdin_set_buffer_size(uint32_t size);
 int mik32_stdin_read(void *__reent, void *dummy, char *dst, int len);
