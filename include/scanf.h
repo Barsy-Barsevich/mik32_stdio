6e2c0405afe1f6d78cb1f471bc5bb8574c8f66cf
Barsy-Barsevich <victor.kinderknecht7@gmail.com>
2025-08-08 18:06:54 +0500

    scanf implementation (raw)

---
+++ include/scanf.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "mik32_stdin.h"
+
+// int fgetc(FILE *file);
+// int getchar(void);
+// int ungetc(int c, FILE *fp);
+// static int idig(int c, int hex);
+// static int iint(FILE *fp, void *dst, int t, int hex, int wid);
+// static int istr(FILE *fp, char *dst, int wid);
+// int vfscanf(FILE *fp, char *fmt, va_list ap);
+// int fscanf(FILE *fp, char *fmt, ...);
+// int scanf(char *fmt, ...);
+// int vsscanf(char *s, char *fmt, va_list ap);
+// int sscanf(char *s, char *fmt, ...);
+// char *fgets(char *s, int sz, FILE *fp);
+// long fread(void *v, long sz, long n, FILE *fp);
+
+#ifdef __cplusplus
+}
+#endif
